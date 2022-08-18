8fccc577415cae99557a3aa683644ca90e4d15fc
kilativ-dotcom <vitalik.tsishchanka@gmail.com>
2022-08-18 16:26:56 +0300

    Change NewsRepository into JpaRepository

--- src/main/java/com/example/news/database/NewsRepository.java
+++ src/main/java/com/example/news/database/NewsRepository.java
@@ -1,9 +1,9 @@
 package com.example.news.database;
 
 import com.example.news.model.News;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NewsRepository extends CrudRepository<News, Long> {
+public interface NewsRepository extends JpaRepository<News, Long> {
     Iterable<News> findAllByOrderByDate();
     boolean existsById(long id);
 }
