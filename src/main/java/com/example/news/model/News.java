0115404a2261529f545a92e7bf664d4468e6f0ce
kilativ-dotcom <vitalik.tsishchanka@gmail.com>
2022-08-23 16:09:52 +0300

    Change GenerationType of News to IDENTITY

--- src/main/java/com/example/news/model/News.java
+++ src/main/java/com/example/news/model/News.java
@@ -7,7 +7,7 @@ import java.util.Date;
 public class News {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 100)
